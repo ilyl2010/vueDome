8041cf4373bdfa2edb2c3de42146f36e6307d60a
zengzhen <zengzhen@si-tech.com.cn>
2020-05-28 21:21:00 +0800

    为ajax demo做准备2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div >
+    <div v-if="!repoUrl">
+      loading
+    </div>
+    <div v-else>
+      most star repo is <a :href="repoUrl">{{repoName}}</a>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
         name: "App",
-
+        data(){
+          return{
+            repoUrl:'',
+            repoName:''
+          }
+        }
     }
 </script>
 
