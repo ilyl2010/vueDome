8716039ae95979f0c33ae22a49799ca912960a5b
zengzhen <zengzhen@si-tech.com.cn>
2020-05-28 21:35:37 +0800

    vue-resource成功调用

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,23 @@
             repoUrl:'',
             repoName:''
           }
-        }
+        },
+      mounted(){
+          //发ajax请求获取数据
+        const url='https://api.github.com/search/repositories?q=vu&sort=stars'
+        this.$http.get(url).then(
+          response => {//成功回调函数
+            console.log(response.data) // 得到返回结果数据
+            const result=response.data;
+            const mostRopo=result.items[0];
+            this.repoUrl=mostRopo.html_url;
+            this.repoName=mostRopo.name;
+          },
+          response => {//失败回调函数
+            console.log(response.data) // 得到返回结果数据
+          },
+        )
+      }
     }
 </script>
 
