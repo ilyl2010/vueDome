9e1e360e348ca01ebfba6f66dbf2af4cad6dbda3
zengzhen <zengzhen@si-tech.com.cn>
2020-06-06 16:41:38 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
-
+<p>click {{count}} times, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data(){
+      return{
+        count:0
+      }
+    },
+    computed:{
+      evenOrOdd(){
+        return this.count%2===0?'偶数':'奇数'
+      }
+    },
+    methods:{
+      increment(){
+        let count=this.count;
+        this.count=++count;
+      },
+      decrement(){
+        const count=this.count;
+        this.count=count-1;
+      },
+      incrementIfOdd(){
+        const count=this.count;
+        if(count%2===1){
+          this.count=count+1;
+        }
+      },
+      incrementAsync(){
+        setTimeout(()=>{
+          let count=this.count;
+          this.count=++count;
+        },1000)
+      },
+    }
+  }
 </script>
 
 <style>
