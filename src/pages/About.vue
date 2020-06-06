6eaf5baed432bba9d373db6ad774204779c0b2c8
zengzhen <zengzhen@si-tech.com.cn>
2020-06-06 10:58:49 +0800

    基础路由

---
+++ src/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>About</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+/*
+路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import About from '../pages/About.vue'
+import Home from '../pages/Home.vue'
+
+// 声明使用vue-router插件
+/*
+内部定义并注册了2个组件标签(router-link/router-view),
+给组件对象添加了2个属性:
+  1. $router: 路由器
+  2. $route: 当前路由
+ */
+Vue.use(VueRouter)
+
+export default new VueRouter ({
+  // 注册应用中所有的路由
+  routes: [
+    {
+      path: '/about',
+      component: About
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/',
+      redirect: '/about'
+    }
+  ]
+})
